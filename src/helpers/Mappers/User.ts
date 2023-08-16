7d901f8f78a653bc7c6063e6fc5f19515c0acc3c
Elyes <elyesmahjoubi@hotmail.com>
2023-08-16 16:10:38 +0000

    removed some entities

--- src/helpers/Mappers/User.ts
+++ src/helpers/Mappers/User.ts
@@ -6,10 +6,12 @@ export class UserMapper implements IMapper<UserEntity, User> {
   GetEntityFromDomain(user: User): UserEntity {
     const record = new UserEntity();
     record.id = user.Id;
-    record.firstName = user.firstName;
-    record.lastName = user.lastName;
-    record.is_active = user.isActive;
+    record.firstName = user.first_name;
+    record.lastName = user.last_name;
+    record.is_active = user.is_active;
+    record.is_admin = user.is_admin;
     record.created_at = user.createdAt;
+    record.company_id = user.company_id;
     record.email = user.email;
     record.password = user.password;
     return record;
@@ -21,7 +23,9 @@ export class UserMapper implements IMapper<UserEntity, User> {
       user_entity.firstName,
       user_entity.lastName,
       user_entity.is_active,
+      user_entity.is_admin,
       user_entity.created_at,
+      user_entity.company_id,
       user_entity.email,
       user_entity.password,
     );
@@ -29,11 +33,13 @@ export class UserMapper implements IMapper<UserEntity, User> {
   }
 
   TransferDomainToEntity(record: UserEntity, user: User): void {
-    record.firstName = user.firstName;
-    record.lastName = user.lastName;
+    record.firstName = user.first_name;
+    record.lastName = user.last_name;
     record.created_at = user.createdAt;
     record.email = user.email;
-    record.is_active = user.isActive;
+    record.is_active = user.is_active;
+    record.is_admin = user.is_admin;
+    record.company_id = user.company_id;
     record.password = user.password;
   }
 }
